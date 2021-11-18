a194d0207b1a69725ee7caf0c22bf6c78c50e24a
Caswell Orr <caswell.orr@gmail.com>
2021-11-17 22:25:52 -0600

    added notes to explain and review the code

--- Develop/script.js
+++ Develop/script.js
@@ -5,20 +5,24 @@
 // use the moment() method to tell the app what time it is so that it will color the timeblocks with the appropriate colors - gray for past, red for present and greren for future
 
 
-
+// create an array of blocks with the 'description' class within the textarea
 let descriptionBlocks = document.querySelectorAll('.description');
 
+// loop through the array of descriptionBlocks 
 for (let i = 0; i < descriptionBlocks.length; i++) {
+  // define the current block
   const currentBlock = descriptionBlocks[i];
-  // console.log(currentBlock);
-  
+
+  // create a variable out of the dataset of the currentBlock in the array to define a time, converting the string into an integer with math.floor so I can add to it below
   let hour = Math.floor(currentBlock.dataset.time);
-  // console.log(hour);
   
+  // create variable for the currentTime using the moment method
   let currentTime = moment();
+  // want to set variables for the beginning and ending of each currentBlock by modifying the moment method - this is so i can then compare the currentTime to the time of the block 
   let startHour = moment().hour(hour).minute(0).second(0);
   let endHour = moment().hour(hour+1).minute(0).second(0);
 
+  // comparing the currentTime and the currentBlocks to assign past, present, or future classes from css that change the color of the blocks
   if (currentTime > endHour){
     console.log("past");
     currentBlock.classList.add('past');
