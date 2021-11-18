3ddef32ecaf85091bb729e8050115fd7736655c1
Caswell Orr <caswell.orr@gmail.com>
2021-11-17 18:38:08 -0600

    added the Timeblocks

---
+++ script.js
@@ -0,0 +1,5 @@
+// Scheduler
+
+// create a bunch of divs that display each hour of the day - AKA "TIMEBLOCKS"
+
+// use the moment() method to tell the app what time it is so that it will color the timeblocks with the appropriate volors - gray for past, red for present and greren for future
