042fcdd288cf29b7330fc07fb91fed5ec353cfe3
Caswell Orr <caswell.orr@gmail.com>
2021-11-18 18:40:35 -0600

    added notes to try to explain my code. I cannot figure out localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,26 @@
-// Scheduler
-
-// create a bunch of divs that display each hour of the day - AKA "TIMEBLOCKS"
-
-// use the moment() method to tell the app what time it is so that it will color the timeblocks with the appropriate colors - gray for past, red for present and greren for future
 
 
-// create an array of blocks with the 'description' class within the textarea
+// ========== PROJECT DESCRIPTION TIMEBLOCKS ==================
+
+
+    // create an array of blocks with the 'description' class within the textarea
 let descriptionBlocks = document.querySelectorAll('.description');
 
-// loop through the array of descriptionBlocks 
+    // loop through the array of descriptionBlocks 
 for (let i = 0; i < descriptionBlocks.length; i++) {
-  // define the current block
+      // define the current block
   const currentBlock = descriptionBlocks[i];
 
-  // create a variable out of the dataset of the currentBlock in the array to define a time, converting the string into an integer with math.floor so I can add to it below
+      // create a variable out of the dataset of the currentBlock in the array to define a time, converting the string into an integer with math.floor so I can add to it below
   let hour = Math.floor(currentBlock.dataset.time);
   
-  // create variable for the currentTime using the moment method
+      // create variable for the currentTime using the moment method
   let currentTime = moment();
-  // want to set variables for the beginning and ending of each currentBlock by modifying the moment method - this is so i can then compare the currentTime to the time of the block 
+      // want to set variables for the beginning and ending of each currentBlock by modifying the moment method - this is so i can then compare the currentTime to the time of the block 
   let startHour = moment().hour(hour).minute(0).second(0);
   let endHour = moment().hour(hour+1).minute(0).second(0);
 
-  // comparing the currentTime and the currentBlocks to assign past, present, or future classes from css that change the color of the blocks
+      // comparing the currentTime and the currentBlocks to assign past, present, or future classes from css that change the color of the blocks
   if (currentTime > endHour){
     console.log("past");
     currentBlock.classList.add('past');
@@ -36,45 +34,51 @@ for (let i = 0; i < descriptionBlocks.length; i++) {
 
   }
     
-    // create time bounds / parameters for currentBlock
-
-    // create two objects (beginning/end of the hour)
-
-    // compare these to currentTime
-
-    // and assign past, present, future based on this comparison
-  
-
-
 }
 
+// ========== STORE DESCRIPTION IN LOCAL STORAGE W/ SAVE BUTTON ==================
 
+
+    // creates variable that is an array of all the save buttons in the html
 let saveBtn = document.querySelectorAll('button');
 
+    // need to loop thru the length of the save button array, then make a variable for a single button (currentBtn) that is the one that will be clicked on
 for (let i = 0; i < saveBtn.length; i++) {
   const currentBtn = saveBtn[i];
 
+    // need an event listener to listen for the click on the current button, ehich is an event - log it to the console - the click event's target is the id within the currentBtn
   currentBtn.addEventListener('click', function(event){
     console.log(event.target.id);
 
-  let timeBlockSaveBtn = event.target.id;
+    // now that i know the eventListener is logging the proper value of the id, i can create a variable that will be the id of the savebtn within a timeblock
+  let timeBlockSaveId = event.target.id;
+    
+    // creates a variable out of whatever is inputed into the text area by grabbing the value of the sibling of the above targeted event?
   let task = $(this).siblings('textarea').val()
-    console.log(task)
+    console.log(task);
+
+  // Vanilla JS not working - returns first value of 9, but doesnt work thru the rest of the loop
+  // let textArea = document.getElementById(`${event.target.dataset.text}`)
+  //   console.log(textArea.value);
   
-    localStorage.setItem(timeBlockSaveBtn, task);
-    getTask()
+  localStorage.setItem(timeBlockSaveId, task);
+
+  getTask();
+    
   })
 
-}
+};
 
 function getTask(){
 
   for (let i = 9; i < 18; i++) {
-    var getTask = localStorage.getItem(i)
-    console.log(task)
+    let getTask = localStorage.getItem(i);
+    console.log(getTask);
+
+
   }
 }
 
 //set task within for loop - set textContrntn of thing with id of i = getTask
 
-// localStorage
+    // console.log(event.target.dataset.text);
